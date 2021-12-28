f2c89b13804295212174e29d73b107567eba7f7e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2021-12-28 13:12:42 -0300

    Update with new scripts and Deployment Features

--- run.sh
+++ run.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 
+HELP="
+Usage: run.sh [OPTIONS]
+
+    -c, --create            Print version information
+    -d, --delete            Configuration file
+    -i, --enable-ingress    Enable ingress
+    -d, --enable-dashboard  Enable dashboard
+    -u, --admin-user        Create admin user
+    -h, --help              Print this help message
+"
+
+
 case $1 in
 
-    --create)
+    -c|--create)
         kind create cluster --config=./config/cluster.yml;;
-    --enable-ingress)
-        kubectl label node --overwrite lab-control-plane ingress-ready=true;;
-        kubectl apply -f ./ingress/nginx.yml;;
-    --delete)
+    -d|--delete)
         kind delete cluster --name $2;;
+    -i|--enable-ingress)
+        kubectl label node --overwrite lab-control-plane ingress-ready=true
+        kubectl apply -f ./ingress/nginx.yml;;
+    -d|--enable-dashboard)
+        kubectl apply -f ./dashboard/deploy.yml;;
+    -u|--admin-user)
+        kubectl apply -f ./dashboard/admin-user.yml;;
+    -h|--help)
+        echo -e "$HELP";;
     *)
-        echo -e "Sorry! Try [--create || --delete <CLUSTER-NAME> ]";;
+        echo -e "$HELP";;
 esac
