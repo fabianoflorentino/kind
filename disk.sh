2b8464f946b21bc69ad04a2a59bfe74764f0615a
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2023-05-15 10:08:21 -0300

    Added constant variable for define size

--- disk.sh
+++ disk.sh
@@ -1,6 +1,11 @@
+#!/bin/sh
+
+LV_SIZE=100G
+
+# Create a 100GB volume on /dev/sdb
 pvcreate /dev/sdb
 vgcreate data /dev/sdb
-lvcreate -n storage -L 100G data
+lvcreate -n storage -L "${LV_SIZE}" data
 mkfs.xfs /dev/data/storage
 mkdir -p /mnt/storage/vol1
 mount /dev/data/storage /mnt/storage/vol1
